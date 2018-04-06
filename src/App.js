9411d33f5b811d28bf69778d877e58bcfe0d5f5c
Kira Helm <soulclimberchick@gmail.com>
2018-04-05 22:26:00 -0700

    Forgot to make a new branch. Assignment 4 List Messages completed

--- src/App.js
+++ src/App.js
@@ -1,32 +1,42 @@
 import React, { Component } from 'react';
-import RoomList from './components/RoomList';
+import RoomList from './components/RoomList.js';
+import MessageList from './components/MessageList.js';
 import * as firebase from 'firebase';
 import './App.css';
 
 // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyASRr9Un36HecJ4y84v5DMzAli4RMVGTgY",
-    authDomain: "bloc-chat-react-e0a88.firebaseapp.com",
-    databaseURL: "https://bloc-chat-react-e0a88.firebaseio.com",
-    projectId: "bloc-chat-react-e0a88",
-    storageBucket: "bloc-chat-react-e0a88.appspot.com",
-    messagingSenderId: "139421148139"
-  };
+var config = {
+  apiKey: 'AIzaSyASRr9Un36HecJ4y84v5DMzAli4RMVGTgY',
+  authDomain: 'bloc-chat-react-e0a88.firebaseapp.com',
+  databaseURL: 'https://bloc-chat-react-e0a88.firebaseio.com',
+  projectId: 'bloc-chat-react-e0a88',
+  storageBucket: 'bloc-chat-react-e0a88.appspot.com',
+  messagingSenderId: '139421148139'
+};
 
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
 class App extends Component {
-  constructor(props) {
+  constructor (props) {
     super(props);
-    this.state = {
-    };
+    this.state = {activeRoom: ''};
+    this.activeRoom = this.activeRoom.bind(this);
   }
 
-  render() {
+  activeRoom (room) {
+    this.setState({ activeRoom: room });
+  }
+
+  render () {
+    const showMessages = this.state.activeRoom;
     return (
-      <div className="App">
-        <RoomList
-          firebase={firebase}/>
+      <div>
+        <h1>{this.state.activeRoom.name || 'Select A Room'}</h1>
+        <RoomList firebase={firebase} activeRoom={this.activeRoom} />
+        { showMessages ?
+        <MessageList firebase={firebase} activeRoom={this.state.activeRoom.key}/>
+        : null
+        }
       </div>
     );
   }
--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,55 +1,54 @@
 import React, { Component } from 'react';
-import './RoomList.css';
+import * as firebase from 'firebase';
 
-class RoomList extends Component {
-  constructor(props) {
+export class RoomList extends Component {
+  constructor (props) {
     super(props);
-    this.state = {
-      rooms: [],
-      newRoomName: "",
-    };
-
+    this.state = { name: '', rooms: [] };
     this.roomsRef = this.props.firebase.database().ref('rooms');
     this.handleChange = this.handleChange.bind(this);
     this.createRoom = this.createRoom.bind(this);
   }
 
-  handleChange(e){
-    this.setState({ newRoomName: e.target.value });
+  handleChange (e) {
+    this.setState({ name: e.target.value });
   }
 
-  createRoom(e) {
+  createRoom (e) {
     e.preventDefault();
-    this.roomsRef.push({ name: this.state.newRoomName });
-    this.setState({ newRoomName: ""});
+    this.roomsRef.push({ name: this.state.name });
+    this.setState({ name: '' });
   }
 
-  componentDidMount() {
+  componentDidMount () {
     this.roomsRef.on('child_added', snapshot => {
       const room = snapshot.val();
       room.key = snapshot.key;
-      this.setState({ rooms: this.state.rooms.concat( room ) })
+      this.setState({ rooms: this.state.rooms.concat(room) });
     });
   }
 
-  render() {
+  selectRoom (room) {
+    this.props.activeRoom(room);
+  }
 
+  render () {
     const roomForm = (
       <form onSubmit={this.createRoom}>
-      <input type="text" value={this.state.newRoomName} placeholder="Enter Room Name" onChange={this.handleChange}/>
-      <input type="submit" value="Create" />
+        <input type='text' value={this.state.name} placeholder='Enter Room Name' onChange={this.handleChange}/>
+        <input type='submit' value='Create' />
       </form>
-    );
+   );
 
     const roomList = this.state.rooms.map((room) =>
-    <li key={room.key}>{room.name}</li>
-  );
+      <li key={room.key} onClick={(e) => this.selectRoom(room, e)}>{room.name}</li>
+   );
 
-    return(
-      <div>
-      <div>{roomForm}</div>
-      <ul>{roomList}</ul>
-      </div>
+    return (
+     <div>
+       <div>{roomForm}</div>
+       <ul>{roomList}</ul>
+     </div>
     );
   }
 }
